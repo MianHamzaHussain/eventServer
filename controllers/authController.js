289ba2c4363a528c6d5ced2808849301cb9449b8
Hamza <mianhamzahussain@gmail.comm>
2023-05-14 23:04:38 +0500

    user-expor

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -10,7 +10,7 @@ const register = async (req, res) => {
     const user = new User({ name, email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-    res.status(201).json({ token,message:"User Registered Succesfuly" });
+    res.status(201).json({ token, message: "User Registered Succesfuly" });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -28,12 +28,12 @@ const login = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-    res.status(200).json({ token,message:"User LoggedIn Succesfuly" });
+    res.status(200).json({ token, message: "User LoggedIn Succesfuly", user });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
-module.exports={
-login,
-register,
-}
+module.exports = {
+  login,
+  register,
+};
