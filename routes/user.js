b0fadb689ce8519d28264c3a8355ee5e155c0557
Hamza <mianhamzahussain@gmail.comm>
2023-05-15 02:53:12 +0500

    Update

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { isAdmin} = require("../middlewares/authorization");
 const { getUsers } = require("../controllers/userController");
-router.get("/privateEvent", isAdmin, getUsers);
+router.get("/user", isAdmin, getUsers);
 module.exports = router;
